ba740ab23b8b5233e5a680e96427ba59e0666325
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2017-05-01 18:48:36 +1000

    fixing error output

--- src/main/scala/spark_etl/MainUtils.scala
+++ src/main/scala/spark_etl/MainUtils.scala
@@ -122,7 +122,7 @@ object MainUtils {
       case Success(_) =>
         log.info("Success!")
       case Failure(errors) =>
-        val errorStr = errors.map(e => e.exc.map(exc => s"${e.msg}, exception: $exc\n${stacktrace(exc)}").getOrElse(e.msg)).toList.mkString("\n• ")
+        val errorStr = errors.map(e => e.exc.map(exc => s"• ${e.msg}, exception: $exc\n${stacktrace(exc)}").getOrElse(e.msg)).toList.mkString("\n")
         log.error(s"Failed due to:\n- $errorStr")
         System.exit(1)
     }
