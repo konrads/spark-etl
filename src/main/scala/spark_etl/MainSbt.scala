f5e29a2eef4a7743d528c7ec9d6a55be404fc7c8
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2017-05-01 14:32:36 +1000

    making workaround executable

--- src/main/scala/spark_etl/MainSbt.scala
+++ src/main/scala/spark_etl/MainSbt.scala
@@ -1,6 +1,6 @@
 package spark_etl
 
 // FIXME: workaround for dodgy sbt target, which doesn't take params.
-object MainSbt {
+object MainSbt extends App {
   Main.main(Array("-Denv.env=__ignore__", "validate-local"))
 }
